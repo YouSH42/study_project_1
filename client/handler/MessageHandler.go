4e5b6d22517d12f5315595286c7fecfc57c93a68
YouSH42 <enddl402@gmail.com>
2023-10-04 18:16:44 +0900

    10/4 tmp

--- client/handler/MessageHandler.go
+++ client/handler/MessageHandler.go
@@ -3,14 +3,7 @@ package handler
 import (
 	"html/template"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/message.html"))
